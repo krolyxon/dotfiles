15264a6db2a2f5f019af989b69d7e0a80625bdca
krolxon <krolyxon@tutanota.com>
2026-01-13 13:08:28 +0530

    nvim: use pywal colorscheme using neopywal

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,44 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        opts = {
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     opts = {
+    --         transparent = true,
+    --         styles = {
+    --             sidebars = "transparent",
+    --             floats = "transparent",
+    --         },
+    --     },
+    -- },
+
+    "RedsXDD/neopywal.nvim",
+    name = "neopywal",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent_background = true,
+        plugins = {
+            alpha = true,
+            dashboard = false,
+            git_gutter = true,
+            indent_blankline = true,
+            lazy = true,
+            lazygit = true,
+            noice = false,
+            notify = true,
+            nvim_cmp = true,
+            mini = {
+                hipatterns = true,
+                indentscope = {
+                    enabled = false,
+                },
+                pick = true,
+                starter = true,
             },
         },
     },
+
+    config = function(_, opts)
+        require("neopywal").setup(opts)
+        vim.cmd.colorscheme("neopywal")
+    end,
 }
