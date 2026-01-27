8b235559a3ef0b3905967bc590337a528ca0b1fa
krolyxon <krolyxon@tutanota.com>
2026-01-27 22:13:07 +0530

    Swaync: use Apple's "Blow" notification sound

--- .config/swaync/notification-sound.sh
+++ .config/swaync/notification-sound.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [ $(swaync-client -D) = "false" ]; then
-	paplay ~/.config/swaync/i-am-loving-it.mp3
+	# paplay ~/.config/swaync/i-am-loving-it.mp3
+	paplay ~/.config/swaync/Blow.aiff
 fi
